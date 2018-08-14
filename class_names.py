5c41a333f9abb8f265c4be51cad1f30e6da15f73
Kenta Murai <muraikenta0507@gmail.com>
2018-08-14 21:19:05 +0900

    Flaskとのつなぎ込み

--- class_names.py
+++ class_names.py
@@ -1,2 +1,25 @@
-class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
-               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+class_names = [
+    'T-shirt/top',
+    'Trouser',
+    'Pullover',
+    'Dress',
+    'Coat',
+    'Sandal',
+    'Shirt',
+    'Sneaker',
+    'Bag',
+    'Ankle boot',
+]
+
+class_names_ja = [
+    'Tシャツ/トップス',
+    'トラウザー',
+    'Pullover',
+    'ドレス',
+    'コート',
+    'サンダル',
+    'シャツ',
+    'スニーカー',
+    'バッグ',
+    'アンクルブーツ',
+]
--- model.py
+++ model.py
@@ -3,25 +3,12 @@ import tensorflow as tf
 from tensorflow import keras
 import numpy as np
 
-# Helper libraries
-# from datetime import datetime
-# import numpy as np
-# import matplotlib.pyplot as plt
-
-# my modules
-# from my_test_images import my_test_images, my_test_lables
-
-def build_model():
+def build_model(epochs=1):
     fashion_mnist = keras.datasets.fashion_mnist
 
     (train_images, train_labels), _ = fashion_mnist.load_data()
 
     train_images = preprocess_data(train_images)
-    # test_images = test_images / 255.0
-    # my_test_images = my_test_images / 255.0
-
-    # class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
-    #                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
     model = keras.Sequential([
         keras.layers.Flatten(input_shape=(28, 28)),
@@ -33,11 +20,10 @@ def build_model():
                   loss='sparse_categorical_crossentropy',
                   metrics=['accuracy'])
 
-    model.fit(train_images, train_labels, epochs=5)
+    model.fit(train_images, train_labels, epochs=epochs)
 
     return model
 
-# TODO: チェッカーの実験
-# def preprocess_data(images: np.ndarray) -> np.ndarray:
+# TODO: typeチェッカーの実験
 def preprocess_data(images: np.ndarray) -> np.ndarray:
     return images / 255.0
